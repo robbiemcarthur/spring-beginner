fa947353572ae9c84ef60de52a33df59afda66bf
Robbie McArthur <2098323m@student.gla.ac.uk>
2021-06-23 19:07:26 +0100

    Restructure

--- conference.demo/src/main/java/com/pluralsight/conference/demo/repositories/SpeakerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.conference.demo.repositories;
-
-import com.pluralsight.conference.demo.models.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
-}
