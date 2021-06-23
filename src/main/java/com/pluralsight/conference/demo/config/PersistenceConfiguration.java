236e302507c7e363c43be1cdaf24cc49767d1051
Robbie McArthur <2098323m@student.gla.ac.uk>
2021-06-23 19:08:15 +0100

    Restructure

---
+++ src/main/java/com/pluralsight/conference/demo/config/PersistenceConfiguration.java
@@ -0,0 +1,22 @@
+package com.pluralsight.conference.demo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+// TODO: Look up auto configuration files, similar to c# class libs - JARs with helper modules etc
+@Configuration
+public class PersistenceConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("Welcome");
+        System.out.println("Custom data source bean initialized and set");
+        return builder.build();
+    }
+}
---
+++ src/main/java/com/pluralsight/conference/demo/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.pluralsight.conference.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    @Value("${app.version}")
+    private String appVersion;
+
+    public HomeController() {
+    }
+
+    @GetMapping
+    @RequestMapping("/")
+    public Map getStatus() {
+        Map map = new HashMap<String, String>();
+        map.put("app-version", appVersion);
+        return map;
+    }
+}
